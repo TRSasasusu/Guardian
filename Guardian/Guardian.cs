531c4b782c7e72b9dd709eb8d081ba5ab4ae29f5
Kaito Kishi <17728957+TRSasasusu@users.noreply.github.com>
2024-10-23 01:31:03 +0900

    initial commit

---
+++ Guardian/Guardian.cs
@@ -0,0 +1,35 @@
+﻿using HarmonyLib;
+using OWML.Common;
+using OWML.ModHelper;
+using System.Reflection;
+
+namespace Guardian {
+    public class Guardian : ModBehaviour {
+        public static Guardian Instance;
+
+        public static void Log(string text, MessageType messageType = MessageType.Message) {
+            Instance.ModHelper.Console.WriteLine(text, messageType);
+        }
+
+        private void Awake() {
+            Instance = this;
+            Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        private void Start() {
+            // Starting here, you'll have access to OWML's mod helper.
+            ModHelper.Console.WriteLine($"{nameof(Guardian)} is loaded!", MessageType.Success);
+
+            // Get the New Horizons API and load configs
+            var newHorizons = ModHelper.Interaction.TryGetModApi<INewHorizons>("xen.NewHorizons");
+            newHorizons.LoadConfigs(this);
+
+            // Example of accessing game code.
+            LoadManager.OnCompleteSceneLoad += (scene, loadScene) => {
+                if (loadScene != OWScene.SolarSystem) return;
+                ModHelper.Console.WriteLine("Loaded into solar system!", MessageType.Success);
+            };
+        }
+    }
+
+}
