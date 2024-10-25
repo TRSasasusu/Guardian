893fdb1f379e2a9fbd8e8a965e8df777533d96a8
Kaito Kishi <17728957+TRSasasusu@users.noreply.github.com>
2024-10-26 02:39:24 +0900

    prepare plasmawarp

--- Guardian/PlasmaWarp.cs
+++ Guardian/PlasmaWarp.cs
@@ -7,6 +7,32 @@ using UnityEngine;
 
 namespace Guardian {
     public class PlasmaWarp : MonoBehaviour {
+        List<Transform> _points;
+        List<ParticleSystem> _beams;
+
+        public void Initialize(IEnumerable<Transform> points) {
+            _points = points.ToList();
+
+            _beams = new List<ParticleSystem>();
+            var plasmaBeam = transform.Find("PlasmaBeam").gameObject;
+            _beams.Add(plasmaBeam.GetComponent<ParticleSystem>());
+            for(var i = 1; i < _points.Count; i++) {
+                var copyPlasmaBeam = Instantiate(plasmaBeam, transform);
+                _beams.Add(copyPlasmaBeam.GetComponent<ParticleSystem>());
+            }
+        }
+
+        void Update() {
+            for(var i = 0; i < _beams.Count; i++) {
+                if(i > 0) {
+                    _beams[i].transform.position = _points[i - 1].transform.position;
+                }
+                _beams[i].transform.LookAt(_points[i]);
+                var mainModule = _beams[i].main;
+                mainModule.startLifetime = Vector3.Distance(_beams[i].transform.position, _points[i].transform.position) * 0.1f;
+            }
+        }
+
         void OnTriggerEnter(Collider other) {
             if(other.gameObject == Locator._playerBody.gameObject) {
                 Patch._canBeDamaged = false;
